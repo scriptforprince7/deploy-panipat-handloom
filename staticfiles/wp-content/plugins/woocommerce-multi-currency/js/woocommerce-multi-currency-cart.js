fcd9b70cd2ad32cd93ccf8c953a58cc545f5465d
Prince Sachdeva <scriptforprince@gmail.com>
2023-10-10 13:15:07 +0530

    First Commit

--- staticfiles/wp-content/plugins/woocommerce-multi-currency/js/woocommerce-multi-currency-cart.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function () {
-	'use strict';
-	if (typeof wc_cart_fragments_params === 'undefined' || wc_cart_fragments_params === null) {
-	} else {
-		sessionStorage.removeItem(wc_cart_fragments_params.fragment_name);
-	}
-});
