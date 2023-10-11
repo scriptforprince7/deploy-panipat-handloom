1f891d022a56cbedbebfe37d43b70ea3f946b158
Prince Sachdeva <scriptforprince@gmail.com>
2023-10-11 12:24:32 +0530

    third deploy

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt 
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Install Python dependencies
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
+# Run Django collectstatic command
+echo "Running collectstatic..."
 python3.9 manage.py collectstatic
+
+echo "Build process completed successfully."
