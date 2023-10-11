c7faa227b6ec20076ac0d68eb1b3b5623dee6574
Prince Sachdeva <scriptforprince@gmail.com>
2023-10-11 13:05:38 +0530

    back-to-normal

--- ecomproj/settings.py
+++ ecomproj/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=gz6u1d6i@v#$c@(zxy_@4+c7w)5hlumrdyu_ru^(_!kc!b5_8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.30', '192.168.248.230', '127.0.0.1', '.vercel.app', 'now.sh']
+ALLOWED_HOSTS = ['192.168.1.30', '192.168.248.230', '127.0.0.1', '*']
 
 
 # Application definition
@@ -84,13 +84,9 @@ WSGI_APPLICATION = "ecomproj.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'IAvoKP0epSiFjVuEMPkw',
-        'HOST': 'containers-us-west-128.railway.app',
-        'PORT': '5758',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
